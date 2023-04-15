b616cb2f5c0f6cd18027065e43dc6bbb1efb3896
LoredanaCicati <loredanacicati@gmail.com>
2023-04-15 13:24:42 +0200

    added sound for enemy death and damage

--- Game/Assets/Scripts/Enemy.cs
+++ Game/Assets/Scripts/Enemy.cs
@@ -6,11 +6,12 @@ using System;
 public class Enemy : MonoBehaviour
 {
     // HP = Hit points - amount of damage that an object can sustain before it is destroyed or defeated
-    public int HP = 100;
+    public int HP = 60;
     public Animator animator;
 
     public void TakeDamage(int damageAmount)
     {
+        Debug.Log("INITIAL"+HP);
         Debug.Log("Enemy taking damage: " + damageAmount + " HP = " + HP);
         HP -= damageAmount;
         if (HP <= 0)
@@ -26,6 +27,7 @@ public class Enemy : MonoBehaviour
     private void Die()
     {
         // Play Death Animation
+        AudioManager.instance.Play("GolemDeath");
         animator.SetTrigger("die");
         GetComponent<Collider>().enabled = false;
         enabled = false;
@@ -34,6 +36,7 @@ public class Enemy : MonoBehaviour
     private void GetHit()
     {
         // Play Get Hit Animation
+        AudioManager.instance.Play("GolemDamage");
         animator.SetTrigger("damage");
     }
 }
