0b218d2245ecb0a78eaa1c4b3ac02859348cbddf
Lia Cicati <LiaCicati@yandex.com>
2023-04-25 11:58:08 +0200

    added script to spawn diamonds randomly

---
+++ Game/Assets/Scripts/DiamondSpawner.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class DiamondSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject diamondPrefab;
+    [SerializeField] private int numberOfDiamonds;
+    [SerializeField] private float spawnRange;
+    [SerializeField] private float spawnHeight;
+
+    private void Start()
+    {
+        for (int i = 0; i < numberOfDiamonds; i++)
+        {
+            Vector3 spawnPosition = transform.position + new Vector3(Random.Range(-spawnRange, spawnRange), 0f, Random.Range(-spawnRange, spawnRange));
+            Instantiate(diamondPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+}
