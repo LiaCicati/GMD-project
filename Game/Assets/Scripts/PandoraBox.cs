aecf69abf04dfad62f70751646eaf532fff03eae
Lia Cicati <LiaCicati@yandex.com>
2023-04-25 17:53:42 +0200

    added spawn box after nr of diamonds are collected

---
+++ Game/Assets/Scripts/PandoraBox.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PandoraBox: MonoBehaviour {
+    private void OnTriggerEnter(Collider other) {
+        // Check if the player has collected all the diamonds
+        PlayerInventory playerInventory = other.GetComponent <PlayerInventory> ();
+        if (playerInventory != null && playerInventory.NumberOfDiamonds >= playerInventory.diamondsNeededForPandoraBox) {
+            // Player has won the game!
+            Debug.Log("Congratulations, you found the Pandora Box!");
+
+            // Destroy the Pandora Box
+            Destroy(gameObject);
+        }
+    }
+}
--- Game/Assets/Scripts/PlayerInventory.cs
+++ Game/Assets/Scripts/PlayerInventory.cs
@@ -9,10 +9,28 @@ public class PlayerInventory : MonoBehaviour
     // Property to get the number of collected diamonds
     public int NumberOfDiamonds { get; private set; }
 
+    // Reference to the Pandora Box prefab
+    public GameObject pandoraBoxPrefab;
+
+    // Number of diamonds needed to spawn the Pandora Box
+    public int diamondsNeededForPandoraBox = 5;
+
+    private bool hasSpawnedPandoraBox = false;
+
+
     // Method called when a diamond is collected
-    public void DiamondCollected()
-    {
-        NumberOfDiamonds++;
-        onDiamondCollected.Invoke(this);
-    }
-}
+ public void DiamondCollected() {
+   NumberOfDiamonds++;
+   onDiamondCollected.Invoke(this);
+
+   // Check if the player has collected enough diamonds to spawn the Pandora Box
+   if (NumberOfDiamonds >= diamondsNeededForPandoraBox && !hasSpawnedPandoraBox) {
+     // Spawn the Pandora Box randomly in the scene
+     Vector3 spawnPosition = new Vector3(Random.Range(-10f, 30f), 0.5f, Random.Range(-10f, 30f));
+     Instantiate(pandoraBoxPrefab, spawnPosition, Quaternion.identity);
+
+     // Set the flag to true to indicate that the Pandora Box has been spawned
+     hasSpawnedPandoraBox = true;
+   }
+ }
+ }
