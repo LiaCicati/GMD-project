493afc3661818dbe4f24f1b8cb50b25afd837f52
Lia Cicati <LiaCicati@yandex.com>
2023-04-27 13:51:11 +0200

    changed updateDiamondText method

--- Game/Assets/Scripts/InventoryUI.cs
+++ Game/Assets/Scripts/InventoryUI.cs
@@ -7,7 +7,7 @@ public class InventoryUI : MonoBehaviour
 {
     // TextMeshProUGUI component to display the number of diamonds
     private TextMeshProUGUI diamondText;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +15,21 @@ public class InventoryUI : MonoBehaviour
         diamondText = GetComponent<TextMeshProUGUI>();
     }
 
-    public void UpdateDiamondText(PlayerInventory playerInventory)
-    {
-        // Set the diamond text to the number of diamonds collected as a string
-        diamondText.text = playerInventory.NumberOfDiamonds.ToString();
-    }
+    public void UpdateDiamondText(PlayerInventory playerInventory) {
+      // Clamp the number of collected diamonds between 0 and the total number of diamonds needed
+      int numCollected = Mathf.Clamp(playerInventory.NumberOfDiamonds, 0, playerInventory.diamondsNeededForPandoraBox);
+
+      // Set the diamond text to show how many diamonds the player has collected out of the total diamonds needed
+      diamondText.text = numCollected.ToString() + "/" + playerInventory.diamondsNeededForPandoraBox.ToString();
+
+      // Check if the player has collected all the required diamonds
+      if (numCollected == playerInventory.diamondsNeededForPandoraBox) {
+        // Set the font
+        diamondText.fontSize = 30;
+
+        // Change the text to prompt the player to find the Pandora Box
+        diamondText.text = "Find the Pandora Box!";
+
+      }
+}
 }
