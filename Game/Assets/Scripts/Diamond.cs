3381e1bb1f7339e00fecf2dfbe9841e9aa10d021
Lia Cicati <LiaCicati@yandex.com>
2023-05-02 20:39:28 +0200

    deactivated remaining diamonds
    remaining diamonds are deactivated to prevent the player from collecting more diamonds and moving to the next mission which is finding the pandora box

--- Game/Assets/Scripts/Diamond.cs
+++ Game/Assets/Scripts/Diamond.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Diamond : MonoBehaviour
 {
@@ -7,7 +8,21 @@ public class Diamond : MonoBehaviour
         PlayerInventory playerInventory = other.GetComponent<PlayerInventory>();
         if (playerInventory != null)
         {
+            // Call the DiamondCollected() method on the PlayerInventory component
             playerInventory.DiamondCollected();
+            if (playerInventory.NumberOfDiamonds == playerInventory.diamondsNeededForPandoraBox)
+            {
+                // Deactivate the game object of the remaining diamonds
+                Diamond[] diamonds = FindObjectsOfType<Diamond>();
+                foreach (Diamond diamond in diamonds)
+                {
+                    if (diamond.gameObject.activeSelf)
+                    {
+                        diamond.gameObject.SetActive(false);
+                    }
+                }
+            }
+            // Deactivate the diamond that was collected
             gameObject.SetActive(false);
         }
     }
