f19befa558347003791ca4da25b19a533da60ee9
LoredanaCicati <loredanacicati@gmail.com>
2023-05-03 17:04:15 +0200

    reformat code

--- Game/Assets/Scripts/Weapon/Bullet.cs
+++ Game/Assets/Scripts/Weapon/Bullet.cs
@@ -5,13 +5,9 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public int damageAmount = 20;
-    void Start()
-    {
-    }
-    
+
     private void OnTriggerEnter(Collider other)
     {
-        //Destroy(transform.GetComponent<Rigidbody>());
         if (other.tag == "Enemy")
         {
             Debug.Log("Bullet hit enemy: " + other.name);
@@ -19,11 +15,5 @@ public class Bullet : MonoBehaviour
             other.GetComponent<Enemy>().TakeDamage(damageAmount);
 
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
